a9c4b99f2d7acfebd9bde9754db92961b2904604
Samson Okeji <engrsamsonokeji@gmail.com>
2020-09-17 21:27:04 +0100

    First commit

---
+++ .history/js/app_20200904185315.js
@@ -0,0 +1,49 @@
+
+// selecting elements
+const nameValue = document.querySelector("#name");
+const latValue = document.querySelector("#lat");
+const lngValue = document.querySelector('#lng');
+const humidityValue = document.querySelector('#humidity')
+const tempValue = document.querySelector('#temp');
+const descValue = document.querySelector('#desc');
+const presValue = document.querySelector('#pressure');
+const windDgValue = document.querySelector("#wind-degree");
+const windSpValue = document.querySelector("#wind-speed");
+
+const inputField = document.querySelector("#input_field");
+const form = document.querySelector('#form');
+
+
+
+
+const GetWeather = (Location) => {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${Location}&appid=914023c54fa48cb8be92d9179794a8ba`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+GetWeather("lagos");
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(inputField.value ,'I am submited')
+
+
+
+
+
+    inputField.value = 
+
+
+}
+
+
+form.addEventListener('submit', handleSubmit);
---
+++ README.md
@@ -0,0 +1,25 @@
+# A Weather Application developed by ENGR. SAMSON ADEIZA OKEJI
+
+Welcome to my weather App project!
+
+The Scope of this project covers:
+    Local Storage
+    Service Workers
+    Web Manifest
+    Push Notifications
+    Cache Storages
+    Lighthouse auditing.
+
+This Project is hosted on  firebase and can be Accessed via the url: https://samsonokeji-weatherapp.web.app/
+
+
+I made use of google chrome browser because it allows the full features of progressive web apps.
+Kindly also use live server, to simulate your app running on a server https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
+and Live sass compiler to compile your sass code https://marketplace.visualstudio.com/items?itemName=ritwickdey.live-sass
+
+Note: I completed this project using HTML5, CSS3 and JavaScript (No JavaScript framework was used!)
+
+
+Please do kindly reach out to me via +2347062682400 or email@ engrsamsonokeji@gmail.com for any enquiries or complaint.
+
+Thank you for Downloading/Viewing my Project!
